9466a487df402f7c55889db9650506c67be067c9
Desmet-Lars <desmetlars5@gmail.com>
2024-11-27 08:50:03 +0100

    added contact page and changed some styles in repository's

--- app/page.js
+++ app/page.js
@@ -2,134 +2,58 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Head from 'next/head';
-import { FaJs, FaReact, FaNodeJs, FaPython, FaCss3Alt, FaHtml5, FaGit, FaGithub, FaAngular, FaJava, FaPhp, FaStar, FaCodeBranch, FaClock } from 'react-icons/fa';
-import { SiNextdotjs } from 'react-icons/si';
+import { motion } from 'framer-motion';
+import Header from '../components/Header';
+import Skills from '../components/Skills';
+import Repositories from '../components/Repositories';
+import Footer from '../components/Footer';
+import SkeletonLoader from '../components/SkeletonLoader';
+import Contact from '../components/Contact';
 
 const Portfolio = () => {
   const [profile, setProfile] = useState(null);
   const [repos, setRepos] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const profileResponse = await axios.get('https://api.github.com/users/Desmet-Lars');
         setProfile(profileResponse.data);
-        console.log(profileResponse.data)
 
-        const reposResponse = await axios.get(`https://api.github.com/users/Desmet-Lars/repos`);
+        const reposResponse = await axios.get('https://api.github.com/users/Desmet-Lars/repos');
         setRepos(reposResponse.data);
       } catch (error) {
-        console.error("Error fetching GitHub data:", error);
+        console.error('Error fetching GitHub data:', error);
       }
     };
 
     fetchData();
   }, []);
 
-  if (!profile) return <p className="text-center text-xl">Loading...</p>;
+  if (!profile) return <SkeletonLoader />;
 
   return (
     <>
-    <div className={`min-h-screen flex flex-col items-center p-8  shadow-lg transition-all duration-300 ${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-50 text-gray-900'}`}>
       <Head>
-        <title>{profile.login}'{profile.login.endsWith('s') ? '' : 's'} GitHub Portfolio</title>
+        <title>{profile.login}'s GitHub Portfolio</title>
+        <meta name="description" content="A professional GitHub portfolio showcasing my projects and skills." />
+        <meta property="og:title" content={`${profile.login}'s GitHub Portfolio`} />
+        <meta property="og:image" content={profile.avatar_url} />
       </Head>
-      <button
-        onClick={() => setDarkMode(!darkMode)}
-        className={`mb-6 p-2 rounded-lg border transition duration-300
-            ${darkMode ? 'text-white border-gray-700 hover:bg-gray-300 hover:text-black' : 'text-black border-gray-300 hover:bg-gray-800 hover:text-white'}`}
-        >
-        Toggle {darkMode ? 'Light' : 'Dark'} Mode
-        </button>
 
-      <h1 className="text-3xl sm:text-5xl font-extrabold mb-4 text-center">{profile.login}'{profile.login.endsWith('s') ? '' : 's'} Portfolio</h1>
-      <img src={profile.avatar_url} alt={`Profile Picture of ${profile.name}`} className="w-32 h-32 rounded-full border-4 border-gray-300 shadow-lg mb-6 dark:border-gray-700" />
-      <p className="text-lg mb-4 italic text-center">{profile.bio || 'No bio available'}</p>
+      {/* Main container with smooth background color transition */}
+      <div className={`min-h-screen ${darkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
+        <Header profile={profile} darkMode={darkMode} toggleTheme={() => setDarkMode(!darkMode)} />
 
-      {/* Skills Section */}
-      <section className="mb-8 w-full">
-        <h2 className="text-2xl sm:text-4xl font-semibold mb-4">Skills</h2>
-        <ul className="grid grid-cols-2 sm:grid-cols-6 gap-4">
-          {[
-            { icon: <FaJs className="text-4xl text-yellow-500 mb-2" />, label: 'JavaScript' },
-            { icon: <FaReact className="text-4xl text-blue-500 mb-2" />, label: 'React' },
-            { icon: <FaNodeJs className="text-4xl text-green-500 mb-2" />, label: 'Node.js' },
-            { icon: <FaPython className="text-4xl text-blue-400 mb-2" />, label: 'Python' },
-            { icon: <FaCss3Alt className="text-4xl text-blue-600 mb-2" />, label: 'CSS' },
-            { icon: <FaHtml5 className="text-4xl text-orange-500 mb-2" />, label: 'HTML' },
-            { icon: <FaGit className="text-4xl text-red-500 mb-2" />, label: 'Git' },
-            { icon: <FaGithub className="text-4xl text-black mb-2" />, label: 'GitHub' },
-            { icon: <FaAngular className="text-4xl text-red-600 mb-2" />, label: 'Angular' },
-            { icon: <FaPhp className="text-4xl text-blue-600 mb-2" />, label: 'PHP' },
-            { icon: <FaJava className="text-4xl text-red-600 mb-2" />, label: 'Java' },
-            { icon: <SiNextdotjs className="text-4xl text-black mb-2" />, label: 'Next.js' }
-          ].map((skill, index) => (
-            <li key={index} className={`flex flex-col items-center p-4 rounded-lg shadow-md ${darkMode ? 'bg-gray-800' : 'bg-gray-100'} hover:shadow-xl transition-shadow duration-300`}>
-              {skill.icon}
-              <span>{skill.label}</span>
-            </li>
-          ))}
-        </ul>
-      </section>
+        <main className="p-8">
+          <Skills darkMode={darkMode} />
+          <Repositories repos={repos} darkMode={darkMode} />
+          <Contact darkMode={darkMode} />
+        </main>
 
-      {/* Repositories Section */}
-      <section className="mb-8 w-full">
-        <h2 className="text-2xl sm:text-4xl font-semibold mb-4">Repositories</h2>
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
-            {repos.map(repo => (
-            <li key={repo.id} className={`p-4 rounded-lg transition-shadow duration-300 shadow-lg ${darkMode ? 'bg-gray-800 hover:bg-gray-700' : 'bg-gray-100 hover:bg-gray-200'}`}>
-                <a
-                href={repo.html_url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600 font-semibold hover:underline text-lg"
-                >
-                {repo.name}
-                </a>
-                <p className="text-gray-600 mt-1">{repo.description || 'No description'}</p>
-                <div className="flex justify-between text-sm text-gray-500 mt-2">
-                <span>
-                    <FaStar className="mb-1 inline-block mr-1 text-yellow-500" />
-                    {repo.stargazers_count}
-                </span>
-                <span>
-                    <FaCodeBranch className="inline-block mr-1 text-green-500" />
-                    {repo.forks_count}
-                </span>
-                <span>
-                    <FaClock className="inline-block mr-1 text-gray-400" />
-                    {new Date(repo.created_at).toLocaleDateString()}
-                </span>
-                </div>
-            </li>
-            ))}
-        </ul>
-      </section>
-
-
-    </div>
-    {/* Footer Section */}
-        <footer className={`w-full p-6 text-center ${darkMode ? 'bg-gray-800 text-gray-300 shadow-lg' : 'bg-gray-200 text-gray-800 shadow-lg'}`}>
-        <p className="mb-4 text-lg">
-            This website is live-updated using the GitHub API to fetch data about my projects and profile.
-        </p>
-        <h2 className="text-2xl font-bold mb-2">Contact Me</h2>
-        <ul className="list-none mb-4 space-y-2">
-            <li>
-            Email: <a href="mailto:desmetlars5@gmail.com" className="text-blue-600 hover:underline">desmetlars5@gmail.com</a>
-            </li>
-            <li>
-            <div className="flex items-center justify-center space-x-2">
-                <FaGithub className="text-4xl text-black mb-2" />
-                <span>
-                    GitHub: <a href="https://github.com/Desmet-Lars" className="text-blue-600 hover:underline">Desmet-Lars</a>
-                </span>
-            </div>
-
-            </li>
-        </ul>
-        <p className="text-sm">© {new Date().getFullYear()} Lars Desmet. All rights reserved.</p>
-        </footer>
+        <Footer profile={profile} darkMode={darkMode} />
+      </div>
     </>
   );
 };
